cd50738725676ec274b95b7f5e0e0933f4146c8b
ihaveint <ihaveint@gmail.com>
2020-02-01 21:40:21 +0330

    Add missing property to Show POCO

--- Models/Show.cs
+++ Models/Show.cs
@@ -7,6 +7,7 @@ namespace Models
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public String Summary { get; set; }
+        public String Title { get; set; }
         public int Price { get; set; }
         public int SalonId { get; set; }
         public Salon Salon { get; set; }
