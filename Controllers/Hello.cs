2cbff777d1323997e59abf3fc037ed345a787246
pylover <vahid.mardani@gmail.com>
2020-02-02 15:10:09 +0330

    Hello Controller

--- Controllers/Hello.cs
+++ Controllers/Hello.cs
@@ -10,8 +10,8 @@ public class ValuesController : ControllerBase
 {
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public ActionResult<string> Get()
     {
-        return new string[] { "value1", "value2" };
+        return "Hello World!";
     }
 }
