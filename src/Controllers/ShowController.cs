e6cfece89d2b30fac3777fb5764559aaaa8c989b
MoeinShafienia <shafienia.moein@gmail.com>
2020-02-13 14:17:13 +0330

    Change response status when startTime is greater than now

--- src/Controllers/ShowController.cs
+++ src/Controllers/ShowController.cs
@@ -20,29 +20,39 @@ namespace Booking.Controllers
         public IActionResult post([FromBody]Show show) {
             
             if (show == null) {
+                Console.WriteLine("1");
                 return BadRequest();
             }
 
             if (show.EndTime == null || show.StartTime == null || show.Title == null ||
                 show.Summary == null || show.Price < 0) 
             {
+                Console.WriteLine("2");
                 return BadRequest();
             }
 
-            if (show.StartTime >= show.EndTime || show.StartTime <= DateTime.Now) {
+            if (show.StartTime <= DateTime.Now) {
+                Console.WriteLine("3");
+                return Conflict();
+            }
+
+            if (show.StartTime >= show.EndTime) {
                 return BadRequest();
             }
 
             if (!_showService.isSalonAvailable(show.SalonId)) {
+                Console.WriteLine("4");
                 return Conflict();
             }
 
             if (show.Summary.Length > 250 || show.Title.Length > 40) {
+                Console.WriteLine("5");
                 return BadRequest();
             }
 
             int maxPrice = 100;
             if (show.Price > maxPrice) {
+                Console.WriteLine("6");
                 return BadRequest();
             }
 
@@ -50,8 +60,9 @@ namespace Booking.Controllers
             int maxShowTime = 120;
             int showLenght = (show.EndTime - show.StartTime).Minutes;
             if (showLenght < minShowTime || showLenght > maxShowTime){
+                Console.WriteLine("7");
                 return BadRequest();
-            }
+            }   
 
             _showService.save(show);
             return Created("show created",show);
