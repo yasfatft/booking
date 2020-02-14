441e15eb515161a94f2ed465d1e5a1b8b0b08ada
yasfatft <yasfatft@gmail.com>
2020-02-14 15:22:36 +0330

    Edit coding style

--- src/Models/Ticket.cs
+++ src/Models/Ticket.cs
@@ -6,7 +6,7 @@ namespace Booking.Models
         public int UserId { get; set; }
         public int SeatId { get; set; }
         public Seat Seat  { get; set; }        
-        public int ShowId { get; set;  }
+        public int ShowId { get; set; }
         public Show Show { get; set; }
     }
 }
