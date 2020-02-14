23d4f20a72c98beb70a4a18c22eb47f88d717e1d
yasfatft <yasfatft@gmail.com>
2020-02-14 15:18:06 +0330

    Edit nullable issue of numeric types

--- src/Controllers/ShowController.cs
+++ src/Controllers/ShowController.cs
@@ -17,51 +17,98 @@ namespace Booking.Controllers
         }
 
         [HttpPost]
-        public IActionResult post([FromBody]Show show) {
+        public IActionResult post([FromBody]RequestShow requestShow) {
             
-            if (show == null) {
-                return BadRequest();
-            }
-
-            if (show.EndTime == null || show.StartTime == null || show.Title == null ||
-                show.Summary == null || show.Price < 0) 
+            
+            if (requestShow == null)
             {
-                return BadRequest();
+                return BadRequest("Null json");
             }
 
-            if (show.StartTime <= DateTime.Now) {
-                return Conflict();
+            if (requestShow.StartTime == null)
+            {
+                return BadRequest("Null StartTime");
             }
 
-            if (show.StartTime >= show.EndTime) {
-                return BadRequest();
+            if (requestShow.EndTime == null)
+            {
+                return BadRequest("Null EndTime");
             }
 
-            if (!isSalonAvailable(show.SalonId)) {
-                return Conflict();
+            if (requestShow.Title == null) 
+            {
+                return BadRequest("Null Title");
             }
 
-            const int maxSummaryLenght = 250;
-            const int maxTitleLenght = 40;
-            if (show.Summary.Length > maxSummaryLenght || show.Title.Length > maxTitleLenght) {
-                return BadRequest();
+            if (requestShow.Summary == null) 
+            {
+                return BadRequest("Null Summary");
+            }
+
+            if (requestShow.Price == null || requestShow.Price < 0) 
+            {
+                return BadRequest("Null Price or negetive Price");
+            }
+
+            if (requestShow.SalonId == null) 
+            {
+                return BadRequest("Null SalonId");
+            }
+
+            if (!isSalonAvailable(requestShow.SalonId ?? -1)) 
+            {
+                return NotFound("Unavailable SalonId");
+            }
+                    
+            if (requestShow.StartTime <= DateTime.Now) 
+            {
+                return BadRequest("StartTime is in past");
+            }
+
+            if (requestShow.StartTime >= requestShow.EndTime) 
+            {
+                return BadRequest("Endtime is before than StartTime");
+            }
+            
+
+            const int maxSummaryLength = 250;
+            const int maxTitleLength = 40;
+            if (requestShow.Title.Length > maxTitleLength)
+            {
+                return BadRequest(string.Format(@"Too long Title, Title must be less than {0} characters", maxTitleLength));
+            }
+            if (requestShow.Summary.Length > maxSummaryLength)
+            {
+                return BadRequest(string.Format(@"Too long Summary, Summary must be less than {0} characters", maxSummaryLength));                
             }
 
             const int maxPrice = 100;
-            if (show.Price > maxPrice) {
-                return BadRequest();
+            if (requestShow.Price > maxPrice) {
+                return BadRequest("Price is over the price threshold");
             }
 
-            const int  minShowTime = 30;
+            const int minShowTime = 30;
             const int maxShowTime = 120;
-            int showLenght = (show.EndTime - show.StartTime).Minutes;
-            if (showLenght < minShowTime || showLenght > maxShowTime){
-                return BadRequest();
+            TimeSpan showLength = new TimeSpan();
+            if ( (requestShow.EndTime - requestShow.StartTime).HasValue )
+            {
+                showLength = ((requestShow.EndTime - requestShow.StartTime)).Value;
+            }
+            int showLengthAsMinute = showLength.Minutes;
+            if (showLengthAsMinute < minShowTime)
+            {
+                return BadRequest("Show period is under minShowTime threshold");
+            }
+            if (showLengthAsMinute > maxShowTime){
+
+                return BadRequest("Show period is over maxShowTime threshold");
             }   
 
+            Show show = RequestShowToShowConverter(requestShow);
+    
             _appDbContext.shows.Add(show);
             _appDbContext.SaveChanges();
-            return Created("show created",show);
+            return Ok(string.Format("Show created {0}",requestShow));
         }
 
         public bool isSalonAvailable(int salonId) {
@@ -73,5 +120,27 @@ namespace Booking.Controllers
             }
             return false;
         }
+        public Show RequestShowToShowConverter(RequestShow requestShow){
+            Show show =new Show();
+            show.StartTime = requestShow.StartTime ?? DateTime.Now;
+            show.EndTime = requestShow.EndTime ?? DateTime.Now;  
+            show.Title = requestShow.Title; 
+            show.Summary = requestShow.Summary;
+            show.Price = requestShow.Price ?? -1;
+            show.SalonId = requestShow.SalonId ?? -1;    
+            return show;     
+        }
     }
+
+    public class RequestShow
+    {
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string Summary { get; set; }
+        public string Title { get; set; }
+        public int? Price { get; set; }
+        public int? SalonId { get; set; 
+        }
+    }
+
 }
